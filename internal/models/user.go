55c16398ef516e91c91faa6ad34c07fa3439eb17
Zuhair Almughlaq <zuhairamahdi@gmail.com>
2024-01-13 15:36:45 +0300

    Remove unnecessary "Salt" field from User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,7 +4,6 @@ type User struct {
 	ID           uint   `json:"id"`
 	Username     string `json:"username"`
 	PasswordHash string `json:"password_hash"`
-	Salt         string `json:"salt"`
 	CreatedAt    string `json:"created_at"`
 	UpdatedAt    string `json:"updated_at"`
 	Active       bool   `json:"active"`
