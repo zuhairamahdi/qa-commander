18de6153ab950b68d65da9ef6c754d25aae02343
Zuhair Abdulla <zuhairamahdi@gmail.com>
2024-08-19 22:34:10 +0300

    add templ and views package

--- handlers/hello.go
+++ handlers/hello.go
@@ -1,16 +1,16 @@
 package handlers
 
 import (
-	"context"
 	"qacommander/views"
 
 	"github.com/labstack/echo/v4"
 )
 
+// Hello renders the Hello component.
 func Hello(c echo.Context) error {
 	component := views.Hello("World")
 
 	// Render the component.
-	return component.Render(context.Background(), c.Response().Writer)
+	return render(c, component)
 
 }
